006c06f8903cc5a42e1046304d07c02005269632
Ochieng424 <ochiengderrick424@gmail.com>
2018-11-11 06:05:05 +0300

    Sign Up UI design complete

---
+++ app/src/main/java/com/example/ochieng_derrick/jhub/SignUpActivity.java
@@ -0,0 +1,13 @@
+package com.example.ochieng_derrick.jhub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
